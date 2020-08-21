9f38792ff3a4e5aa03efddd05afe861c38303eb9
Alma_Sesion1 <alma.barron@sygno.com>
2020-08-21 09:53:39 -0500

    cambio

--- Program.cs
+++ Program.cs
@@ -4,10 +4,9 @@ namespace demoRevert
 {
     class Program
     {
-        public string name {get; set;}
+         public int edad {get; set;}
         static void Main(string[] args)
-        {
-            public int edad {get; set;}
+        {           
             Console.WriteLine("Hello World!");
         }
     }
